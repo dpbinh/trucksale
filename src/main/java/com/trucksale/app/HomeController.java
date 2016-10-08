de23236fe8e8909f007a83312999407c7c40bbfd
dpbinh <dpbinh252@gmail.com>
2016-10-08 08:25:04 +0700

    Revert "Revert "init""
    This reverts commit cd57290010742c4027d31b4bda7080a6ab6eb887.

---
+++ src/main/java/com/trucksale/app/HomeController.java
@@ -0,0 +1,19 @@
+package com.trucksale.app;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+
+@Controller
+public class HomeController {
+	private Log log = LogFactory.getLog(this.getClass());
+	
+    @RequestMapping(value = "/home", method=RequestMethod.GET)
+	public String home() {
+    	log.info("home visited");
+	    return "site.homepage";
+	}
+}
